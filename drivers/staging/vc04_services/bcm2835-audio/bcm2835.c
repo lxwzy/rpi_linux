48565b213963f43194eb3e26593d3f961795401e
Wu Zhiyuan <git@wuzhiyuan.com>
2023-11-06 22:40:39 +0800

    bcm2835-audio: removed  enable_compat_alsa 143b67f19ba1d72b010e6fb3d254d36a86d2bc03

--- drivers/staging/vc04_services/bcm2835-audio/bcm2835.c
+++ drivers/staging/vc04_services/bcm2835-audio/bcm2835.c
@@ -12,7 +12,6 @@
 
 static bool enable_hdmi = true, enable_hdmi0, enable_hdmi1;
 static bool enable_headphones = true;
-static bool enable_compat_alsa;
 static int num_channels = MAX_SUBSTREAMS;
 
 module_param(enable_hdmi, bool, 0444);
